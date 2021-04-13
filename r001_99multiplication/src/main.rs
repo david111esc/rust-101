33f9c2bfb8ed6eaf5510bd3f8f77fb8b05c02f8d
david111esc <david111esc@gmail.com>
2021-04-13 23:27:52 +0800

    feat: complete project

--- r001_99multiplication/src/main.rs
+++ r001_99multiplication/src/main.rs
@@ -7,9 +7,10 @@ fn intro() {
 
 fn input_process() -> u16 {
     loop {
+        intro();
         let mut input: String = String::new();
         stdin().lock().read_line(&mut input).unwrap();
-        println!("{} ", input);
+        println!("Your input is {}.", input);
         if input.get(..input.len() - 2).unwrap().parse::<u8>().is_ok() {
             println!("SUCCESS!");
             return input
@@ -18,11 +19,35 @@ fn input_process() -> u16 {
                 .parse::<u16>()
                 .unwrap();
         }
+        println!("It is invalid input!");
+    }
+}
+
+fn display(n: u16) {
+    let w: usize = n.to_string().len();
+    let w2: usize = 2 * w;
+    for i in 1..=n {
+        for j in 1..n {
+            print!(
+                "{:>width$} x {:^width$} = {:>width2$}, ",
+                j,
+                i,
+                i * j,
+                width = w,
+                width2 = w2
+            );
+        }
+        println!(
+            "{:>width$} x {:^width$} = {:>width2$}",
+            n,
+            i,
+            i * n,
+            width = w,
+            width2 = w2
+        );
     }
 }
 
 fn main() {
-    intro();
-    println!("{}", input_process());
-    intro();
+    display(input_process());
 }
